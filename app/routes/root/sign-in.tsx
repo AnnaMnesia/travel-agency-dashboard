ce819ee8eda60efed21a045c6e27b3e19ab89c06
AnnaMnesia <annamast@windowslive.com>
2026-01-19 12:40:30 +0100

    Implement Routing, Dashboard Layout & UI, Auth Functionality

---
+++ app/routes/root/sign-in.tsx
@@ -0,0 +1,9 @@
+const SignIn = () => {
+  return (
+    <main className="auth">
+      <section className="size-full glassmorphism flex-center px-6"></section>
+    </main>
+  );
+};
+
+export default SignIn;
